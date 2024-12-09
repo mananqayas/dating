ac465909ba7c71be56769ffdc648bccc68f90a65
mananqayas <mananqayas960@gmail.com>
2024-12-09 15:07:20 -0500

    client

--- API/Program.cs
+++ API/Program.cs
@@ -7,13 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt=>{
-
+builder.Services.AddCors();
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
